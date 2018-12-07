ca7cc34de006dd2f065b8c1e7f638a9a757ff2dc
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-07 16:51:11 -0500

    add mouseover effect function

--- js/etch.js
+++ js/etch.js
@@ -1,11 +1,23 @@
-const gridBody = document.querySelector('.container');
-const gridBtn = document.querySelector('#create-grid');
+const gridBody  = document.querySelector('.container');
+const gridBtn   = document.querySelector('#create-grid');
+let box;
+
 gridBtn.addEventListener('click', createGrid);
 
 function createGrid() {
     for (let i = 0; i < 100; i++) {
-        let box = document.createElement('div');
+        box = document.createElement('div');
         box.classList.add('card');
+        box.style.border = '2px solid black';
         gridBody.appendChild(box);
     }
 }
+
+const gridBoxes = document.querySelectorAll('.card');
+
+Array.from(gridBoxes).forEach
+    (el => el.addEventListener('mouseover', () => {
+        console.log('test');
+        el.style.backgroundColor = 'black';
+    })
+)
