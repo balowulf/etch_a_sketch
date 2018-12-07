bc152b356bd3e1c3efc9b549a3ac6e0ad0413eeb
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-06 20:19:33 -0500

    Initial Commit

---
+++ js/etch.js
@@ -0,0 +1,15 @@
+const gridBody = document.querySelector('.container');
+const gridBtn = document.querySelector('#create-grid');
+gridBtn.addEventListener('click', createGrid);
+
+function createGrid() {
+    for (let i = 0; i < 100; i++) {
+        let box = document.createElement('div');
+        box.classList.add('card');
+        gridBody.appendChild(box);
+    }
+}
+
+function test() {
+    console.log('Hey!');
+}
