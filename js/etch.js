14c481843dd86e9480895113754b5de73ca12aef
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-08 20:06:45 -0500

    refactored all files, working grid creation and etching effect

--- js/etch.js
+++ js/etch.js
@@ -1,22 +1,20 @@
 const gridBody  = document.querySelector('.container');
 const gridBtn   = document.querySelector('#create-grid');
-const gridBoxes = document.querySelectorAll('div');
+const gridBoxes = document.querySelectorAll('.box');
 let box;
 
 gridBtn.addEventListener('click', createGrid);
 
 function createGrid() {
-    for (let i = 0; i < 1482; i++) {
+    for (let i = 0; i < 2916; i++) {
         box = document.createElement('div');
-        box.classList.add('card');
-        box.style.border = '1px solid black';
+        box.classList.add('box');
+        box.style.border = '1px solid darkgray';
         gridBody.appendChild(box);
     }
 }
 
-Array.from(gridBoxes).forEach
-    (el => el.addEventListener('click', () => {
-        console.log(el);
-        el.style.backgroundColor = 'black';
-    })
-)
+gridBody.addEventListener('mouseover', (e) => {
+    e.target.classList.add('orange');
+    console.log(e.target);
+})
