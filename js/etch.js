72e475917785d69ff99b615dd5a92c47a269a9a1
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-09 13:43:18 -0500

    implement user option to choose grid size

--- js/etch.js
+++ js/etch.js
@@ -1,31 +1,41 @@
-const gridBody  = document.querySelector('.container');
-const gridBtn   = document.querySelector('#create-grid');
-// const gridBoxes = document.querySelectorAll('div.box');
-// const gridBoxes = document.getElementsByClassName('box');
-const gridBoxes = document.getElementsByClassName('box');
-const clearBtn  = document.querySelector('#clear');
+const gridBody     = document.querySelector('.container');
+const gridBtn      = document.querySelector('#create-grid');
+const clearBtn     = document.querySelector('#clear');
+const border       = '1px solid darkgray';
+const gridBodySize = 650;
 let box;
 
 gridBtn.addEventListener('click', createGrid);
 clearBtn.addEventListener('click', clearGrid);
-
-function createGrid() {
-    for (let i = 0; i < 2916; i++) {
-        box = document.createElement('div');
-        box.classList.add('box');
-        box.style.border = '1px solid darkgray';
-        gridBody.appendChild(box);
-    }
-}
-
 gridBody.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('container')) {
         return;
     } else {
         e.target.classList.add('orange');
-        console.log(e.target);
     }
 })
+
+function createGrid() {
+    let gridSize = prompt('What size grid would you like?');
+    if (gridSize > 100) {
+        alert(`That's too big!`);
+        return;
+    } else if (gridSize < 2) {
+        alert(`That's too small!`);
+        return;
+    }
+    let boxSize = gridBodySize / gridSize;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        box = document.createElement('div');
+        box.classList.add('box');
+        box.setAttribute('style', 
+            `border: ${border};
+             height: ${boxSize}px;
+             width : ${boxSize}px;`
+        )
+        gridBody.appendChild(box);
+    }
+}
 
 function clearGrid() {
     while (gridBody.firstChild) {
