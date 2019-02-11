971e666fd101052a3c70c3768d09933571f9d200
Chris Balow Jr <cmbalow@illuminateed.net>
2019-02-11 15:44:07 -0500

    combine event btns into single controller btn

--- js/etch.js
+++ js/etch.js
@@ -1,44 +1,63 @@
+/********** UI Variables **********/
+
+const controller   = document.querySelector('.controller');
 const gridBody     = document.querySelector('.container');
-const gridBtn      = document.querySelector('#create-grid');
-const clearBtn     = document.querySelector('#clear');
+const gridBtn      = document.querySelector('.create-grid');
 const border       = '1px solid darkgray';
 const gridBodySize = 650;
 let box;
 
-gridBtn.addEventListener('click', createGrid);
-clearBtn.addEventListener('click', clearGrid);
+/********** Event Listeners **********/
+
+gridBtn.addEventListener('click', (e) => {
+  if (controller.classList.contains('create-grid')) {
+    createGrid();
+  } else {
+    clearGrid();
+  }
+});
+
 gridBody.addEventListener('mouseover', (e) => {
-    if (e.target.classList.contains('container')) {
-        return;
-    } else {
-        e.target.classList.add('orange');
-    }
+  if (e.target.classList.contains('container')) {
+    return;
+  } else {
+    e.target.classList.add('orange');
+  }
 })
 
+/********** Functions **********/
+
 function createGrid() {
-    let gridSize = prompt('What size grid would you like?');
-    if (gridSize > 100) {
-        alert(`That's too big!`);
-        return;
-    } else if (gridSize < 2) {
-        alert(`That's too small!`);
-        return;
-    }
-    let boxSize = gridBodySize / gridSize;
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        box = document.createElement('div');
-        box.classList.add('box');
-        box.setAttribute('style', 
-            `border: ${border};
-             height: ${boxSize}px;
-             width : ${boxSize}px;`
-        )
-        gridBody.appendChild(box);
-    }
+  controller.classList.remove('create-grid');
+  controller.textContent = 'Clear';
+  let gridSize = prompt('What size grid would you like?');
+  if (gridSize === '') {
+    alert(`Please enter a value between 2 and 100`);
+    return;
+  } else if (gridSize > 100) {
+    alert(`That's too big!`);
+    return;
+  } else if (gridSize < 2) {
+    alert(`That's too small!`);
+    return;
+  }
+  let boxSize = gridBodySize / gridSize;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    box = document.createElement('div');
+    box.classList.add('box');
+    box.setAttribute('style', 
+      `border: ${border};
+        height: ${boxSize}px;
+        width : ${boxSize}px;`
+    )
+    gridBody.appendChild(box);
+  }
 }
 
 function clearGrid() {
-    while (gridBody.firstChild) {
-        gridBody.removeChild(gridBody.firstChild);
-    }
+  while (gridBody.firstChild) {
+    gridBody.removeChild(gridBody.firstChild);
+  }
+  controller.textContent = 'Create Grid';
+  controller.classList.add('create-grid');
 }
