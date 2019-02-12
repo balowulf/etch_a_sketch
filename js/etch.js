384cef4efbfeb74a2324e20453e4de22b63172af
Chris Balow Jr <cmbalow@illuminateed.net>
2019-02-12 10:05:48 -0500

    functionality improvements and error handling

--- js/etch.js
+++ js/etch.js
@@ -1,21 +1,27 @@
 /********** UI Variables **********/
 
-const controller   = document.querySelector('.controller');
-const gridBody     = document.querySelector('.container');
-const gridBtn      = document.querySelector('.create-grid');
-const border       = '1px solid darkgray';
-const gridBodySize = 650;
+const controllerContainer = document.querySelector('.controller-container');
+const controller          = document.querySelector('.controller');
+const gridBody            = document.querySelector('.container');
+const gridBtn             = document.querySelector('.create-grid');
+const button              = document.querySelector('button');
+const border              = '1px solid darkgray';
+const gridBodySize        = 650;
 let box;
+
+button.style.display = 'none';
 
 /********** Event Listeners **********/
 
-gridBtn.addEventListener('click', (e) => {
+controller.addEventListener('click', (e) => {
   if (controller.classList.contains('create-grid')) {
-    createGrid();
+    gridPrompt();
   } else {
     clearGrid();
   }
 });
+
+button.addEventListener('click', createGrid);
 
 gridBody.addEventListener('mouseover', (e) => {
   if (e.target.classList.contains('container')) {
@@ -23,25 +29,18 @@ gridBody.addEventListener('mouseover', (e) => {
   } else {
     e.target.classList.add('orange');
   }
-})
+});
 
 /********** Functions **********/
 
 function createGrid() {
-  controller.classList.remove('create-grid');
-  controller.textContent = 'Clear';
-  // prompt and validation for gridsize
-  let gridSize = prompt('What size grid would you like?');
-  if (gridSize === '' || isNaN(gridSize)) {
-    alert(`Please enter a value between 2 and 100`);
-    return;
-  } else if (gridSize > 100) {
-    alert(`That's too big!`);
-    return;
-  } else if (gridSize < 2) {
-    alert(`That's too small!`);
+  let gridSize = parseInt(controller.value);
+  if (isNaN(gridSize) || gridSize > 100 || gridSize < 2) {
+    clearGrid();
     return;
   }
+  controller.placeholder = 'Clear';
+  button.style.display = 'none';
   // create grid based on gridsize input
   let boxSize = gridBodySize / gridSize;
   for (let i = 0; i < gridSize * gridSize; i++) {
@@ -49,17 +48,26 @@ function createGrid() {
     box.classList.add('box');
     box.setAttribute('style', 
       `border: ${border};
-       height: ${boxSize}px;
-       width : ${boxSize}px;`
+        height: ${boxSize}px;
+        width : ${boxSize}px;`
     )
     gridBody.appendChild(box);
   }
+  controller.value = null;
+};
+
+function gridPrompt() {
+  controller.classList.remove('create-grid');
+  controller.placeholder = 'Enter a number (2 - 100)';
+  button.style.display = 'block';
 }
 
 function clearGrid() {
+  controller.value = null;
+  controller.placeholder = 'Start';
+  controller.classList.add('create-grid');
+  button.style.display = 'none';
   while (gridBody.firstChild) {
     gridBody.removeChild(gridBody.firstChild);
   }
-  controller.textContent = 'Create Grid';
-  controller.classList.add('create-grid');
 }
